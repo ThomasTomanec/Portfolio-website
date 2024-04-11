c8e77ba4401b9152243ca3dcbe89838a0d596adb
ThomasTomanec <tomasttomanec@gmail.com>
2024-04-12 01:19:05 +0200

    update-1.1.6

---
+++ app/api/users/[username].ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import fetch from 'node-fetch';
+
+const users = async (req: Request, res: Response): Promise<void> => {
+  const { username } = req.query;
+
+  const response = await fetch('https://api.github.com/users/' + username, {
+    headers: { Authorization: `Bearer ${process.env.GH_TOKEN}` },
+  });
+  const data = await response.json();
+
+  res.status(200).json(data);
+};
+
+export default users;
