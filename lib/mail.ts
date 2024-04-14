ce6e78174a27e6ce6ddf95433fe8523212d59fcc
ThomasTomanec <tomasttomanec@gmail.com>
2024-04-15 01:11:04 +0200

    update-1.2.0

--- lib/mail.ts
+++ lib/mail.ts
@@ -6,7 +6,7 @@ export const sendPasswordResetEmail = async (
     email: string,
     token: string,
 ) => {
-    const resetLink = `http://localhost:3000/auth/new-password?token=${token}`
+    const resetLink = `http://portfolio.thomastomanec.cz/auth/new-password?token=${token}`
 
     await resend.emails.send({
         from: "reset_tomanec@thomastomanec.cz",
@@ -20,7 +20,7 @@ export const sendVerificationEmail = async (
     email: string,
     token: string
 ) => {
-    const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+    const confirmLink = `http://portfolio.thomastomanec.cz/auth/new-verification?token=${token}`;
 
     await resend.emails.send({
         from: "verification_tomanec@thomastomanec.cz",
